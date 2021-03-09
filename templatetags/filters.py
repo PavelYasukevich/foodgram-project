3d4e9273327fee6f5c4d22cb3ed5e8f5c3d189c0
PavelYasukevich <ptntbox@gmail.com>
2021-03-09 14:26:26 +0500

    render tag combined for new and edit recipe

--- templatetags/filters.py
+++ templatetags/filters.py
@@ -11,24 +11,13 @@ def addclass(field, css):
 
 
 @register.inclusion_tag('recipes/aux/render_tags.html')
-def render_recipe_form_tags(items):
+def render_tags(items, **kwargs):
+    recipe = kwargs.get('recipe', None)
+    if recipe is not None:
+        recipe_tags = list(recipe.tags.values_list('id', flat=True))
     for item in items:
         _id = item.data['value'].value
-        if _id == 1:
-            item.data['attrs']['class'] = 'tags__checkbox tags__checkbox_style_orange'
-        if _id == 2:
-            item.data['attrs']['class'] = 'tags__checkbox tags__checkbox_style_green'
-        if _id == 3:
-            item.data['attrs']['class'] = 'tags__checkbox tags__checkbox_style_purple'
-    return {'items': items}
-
-
-@register.inclusion_tag('recipes/aux/render_tags.html')
-def render_recipe_edit_tags(items, recipe):
-    recipe_tags = list(recipe.tags.values_list('id', flat=True))
-    for item in items:
-        _id = item.data['value'].value
-        if _id in recipe_tags:
+        if recipe is not None and _id in recipe_tags: 
             item.data['attrs']['checked'] = True
         if _id == 1:
             item.data['attrs']['class'] = 'tags__checkbox tags__checkbox_style_orange'
