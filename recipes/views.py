6e40d09aaa39d31c4c04e965cf5dc4bbcc4774b0
PavelYasukevich <ptntbox@gmail.com>
2021-03-13 21:35:30 +0500

    anon user checklist done

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
