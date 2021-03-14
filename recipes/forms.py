25b433227dde33fbd32ff4d5db4f00f423aee31c
PavelYasukevich <ptntbox@gmail.com>
2021-03-14 23:46:07 +0500

    imagefile widget, capfirsts, links to recipe from subs, purchases

--- recipes/forms.py
+++ recipes/forms.py
@@ -3,10 +3,17 @@ from django import forms
 from .models import Amount, Ingredient, Recipe, Tag
 
 
+class ImageFieldWidget(forms.ClearableFileInput):
+    template_name = 'recipes/aux/imagefield_widget.html'
+
+
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name', 'cooking_time', 'description', 'image']
+        widgets = {
+            'image': ImageFieldWidget(),
+        }
 
 
 class FilterForm(forms.Form):
